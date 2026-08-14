df6c3e5a17719c5371b4338a26778469fbc74a0e
Thiago Gonzaga <thiago.gonzaga@icloud.com>
2026-08-14 00:33:23 -0300

    Give the library API cluster start, and thin the start command onto it

--- Sources/ContainerK8s/Commands/K8sStart.swift
+++ Sources/ContainerK8s/Commands/K8sStart.swift
@@ -37,38 +37,7 @@ public struct K8sStart: AsyncParsableCommand {
         LoggingSystem.bootstrap { _ in StderrLogHandler() }
         let log = Logger(label: K8sHelper.pluginName)
 
-        let client = ContainerClient()
-        let container = try await client.get(id: name)
-
-        guard container.configuration.labels[ResourceLabelKeys.plugin] == K8sHelper.pluginName else {
-            log.error("container is not a k8s cluster, refusing start", metadata: ["name": "\(name)"])
-            throw ContainerizationError(.invalidArgument, message: "\(name) is not a k8s cluster")
-        }
-
-        if container.status == .running {
-            print(name)
-            return
-        }
-
-        let io = try ProcessIO.create(tty: false, interactive: false, detach: true)
-        defer { try? io.close() }
-        let process = try await client.bootstrap(id: name, stdio: io.stdio)
-        try await process.start()
-        try io.closeAfterStart()
-
-        try await K8sHelper.waitForNodeBooted(containerId: name, client: client, log: log)
-        try await K8sHelper.waitForReady(containerId: name, client: client, log: log)
-
-        do {
-            let fqdn = await K8sHelper.detectFQDN(name: name)
-            let rawConfig = try await K8sHelper.fetchConfig(containerId: name, client: client, log: log)
-            let config = try await K8sHelper.transformConfig(rawConfig, containerId: name, fqdn: fqdn, client: client)
-            try K8sHelper.mergeConfig(config, containerId: name, log: log)
-        } catch {
-            log.warning("failed to write kubeconfig", metadata: ["name": "\(name)", "error": "\(error)"])
-            log.info("cluster is running; use 'container k8s write-config --name \(name)' to write the kubeconfig")
-        }
-
+        try await K8sClusters.start(name: name, log: log)
         print(name)
     }
 }
